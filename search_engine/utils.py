08da9a385b058858985451fdc2dfa2b060c21847
Sadhana J <sadhana.j@SW-LP03679-3.local>
2020-05-02 17:29:11 +0530

    added comment

--- search_engine/utils.py
+++ search_engine/utils.py
@@ -1,3 +1,6 @@
+"""
+utils file, contains code which are generic
+"""
 from django.core.management import call_command
 
 
@@ -7,9 +10,11 @@ def is_empty_or_null(value):
     return False
 
 
+# run elastic search command to rebuild index
 def rebuild_elasticsearch_index():
     call_command('search_index', '--rebuild', '-f')
 
 
+# run elastic search command to delete index
 def delete_elasticsearch_index():
     call_command('search_index', '--delete', '-f')
