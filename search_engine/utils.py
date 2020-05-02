342a224751a8e51b4c15fa18d3e7439c24ed1f13
Sadhana J <sadhana.j@SW-LP03679-3.local>
2020-05-02 17:26:37 +0530

    handle delete elasticsearch index

--- search_engine/utils.py
+++ search_engine/utils.py
@@ -9,3 +9,7 @@ def is_empty_or_null(value):
 
 def rebuild_elasticsearch_index():
     call_command('search_index', '--rebuild', '-f')
+
+
+def delete_elasticsearch_index():
+    call_command('search_index', '--delete', '-f')
