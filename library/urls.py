0a716169003e8c3681d16aa85a25dd1a7d4d283e
Sadhana J <sadhana.j@SW-LP03679-3.local>
2020-05-02 08:20:59 +0530

    removed unused django-admin url

--- library/urls.py
+++ library/urls.py
@@ -14,26 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.urls import path
 
 from search_engine import urls as search_engine_urls
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('search_engine/', include(search_engine_urls)),
 ]
 
-
+# if DEBUG set as True then build debuger url
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
 
     ] + urlpatterns
